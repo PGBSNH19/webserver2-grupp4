4b91eadb068b2581070cdddbcd7b63d7bed6c74c
Adgnascor <william.lindahl@live.com>
2020-09-01 13:22:05 +0200

    Change the code, hopefullr more correct and not just copied

--- Src/MiniWebServer/MiniWebServer/Program.cs
+++ Src/MiniWebServer/MiniWebServer/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.IO;
 using System.Net;
+using System.Net.Http;
+using System.Resources;
 
 namespace MiniWebServer
 {
     class Program
     {
-        public string Value { get; set; }
         static void Main(string[] args)
         {
             string[] test = new string[1];
@@ -15,12 +15,6 @@ namespace MiniWebServer
             SimpleListenerExample(test);
         }
 
-        public static string NextCustomerID()
-{
-    // A real-world application would do something more robust
-    // to ensure uniqueness.
-    return DateTime.Now.ToString();
-}
         public static string ReturnIndexHTML()
         {
             string path = @"C:\Users\willi\Dev\webserver2-grupp4\Content\index.html";
@@ -31,6 +25,13 @@ namespace MiniWebServer
 
         public static void SimpleListenerExample(string[] prefixes)
         {
+            CookieCollection cookies = new CookieCollection();
+            Cookie cookieTime = new Cookie();
+
+            cookieTime.Expires.Add(new TimeSpan(0, 1, 0));
+            cookieTime.Value = DateTime.Now.ToString();
+            
+            cookies.Add(cookieTime);
             if (!HttpListener.IsSupported)
             {
                 Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
@@ -48,35 +49,26 @@ namespace MiniWebServer
             }
             listener.Start();
             Console.WriteLine("Listening...");
-
             HttpListenerContext context = listener.GetContext();
             HttpListenerRequest request = context.Request;
-            string customerID = null;
-
-            Cookie cookie = request.Cookies["ID"];
-            if (cookie != null)
-                customerID = cookie.Value;
-            if (customerID != null)
-                Console.WriteLine("Found the Cookie!");
-
             HttpListenerResponse response = context.Response;
 
-            if(customerID == null)
-            {
-                customerID = NextCustomerID();
-                Cookie cook = new Cookie("ID", customerID);
-                response.AppendCookie(cook);
-            }
-
-            string responseString = ReturnIndexHTML() + customerID;
+            response.Cookies.Add(cookieTime);
+            cookies.Add(response.Cookies);
+            
+            string responseString = ReturnIndexHTML() + cookies.Count;
             byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
 
+            
             response.ContentLength64 = buffer.Length;
+            
             System.IO.Stream output = response.OutputStream;
             output.Write(buffer, 0, buffer.Length);
 
             output.Close();
             listener.Stop();
+
+
         }
     }
 }
